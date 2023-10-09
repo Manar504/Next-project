55f3fa6e80d6c61c75c0616747d0056efbbb2551
Manar_Mamdouh <131297437+Manar504@users.noreply.github.com>
2023-10-09 03:54:51 +0300

    manar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,35 +1,53 @@
-import Link  from 'next/link';
-import React from 'react' ;
-import Image from 'next/image';
-import { NavLinks } from '@/constant';
+import Image from "next/image";
+import Link from "next/link";
 
-const Navbar = () => {
+import { NavLinks } from "@/constant";
+import { getCurrentUser } from "@/lib/session";
+
+import AuthProviders from "./AuthProviders";
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
+
+const Navbar = async () => {
+  const session = await getCurrentUser()
+
   return (
     <nav className='flexBetween navbar'>
-        <div className='flex-1 flexStart gap-10'> 
-        <Link href="/">
-            <Image  
-            src="/logo.svg"
-            width={115}
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image
+            src='/logo.svg'
+            width={116}
             height={43}
-            alt='Flexibble'
-            
-            />
-        </Link> 
-        <ul className='xl:flex hidden text-small gap-7'> 
-    {NavLinks.map((link)=>(
-    <Link href={link.href} key={link.key}
-        > {link.text}</Link>
-    ))}
-
+            alt='logo'
+          />
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.text}>
+              {link.text}
+            </Link>
+          ))}
         </ul>
+      </div>
 
-        </div> 
-<div className='flexCenter gap-4'>
+      <div className='flexCenter gap-4'>
+        {session?.user ? (
+          <>
+            <ProfileMenu session={session} />
 
-</div>
+            <Link href="/create-project">
+              <Button title='Share work' />
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+
